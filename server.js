44e5d88655af2b5014387471d09e97a3de417cd9
hilbug <65197724+hilbug@users.noreply.github.com>
2020-08-29 22:20:33 -0400

    set-up express server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 // set-up express server
+
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+//run 'npm start' to start the server
+//https://medium.com/@ralph1786/how-to-setup-an-express-server-5fd9cd9ae073
