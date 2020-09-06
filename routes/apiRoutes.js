b1e5031e2870b2fe6c100606193a55e9a979322b
hilbug <65197724+hilbug@users.noreply.github.com>
2020-09-05 23:05:47 -0400

    delete notes working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -48,6 +48,9 @@ apiRouter.post("/api/notes", (req, res) => {
             if (err) throw err;
             console.log("The new note was appended to the file!");
         });
+        
+        // return parsed version of new stringified file to read back to the page
+        // adding this resolved ERR EMPTY RESPONSE
         return res.send(JSON.parse(newStringifiedFile));
     });    
 });
@@ -57,6 +60,34 @@ apiRouter.post("/api/notes", (req, res) => {
 //receive a query parameter containing the id of a note to delete.
 // In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 //apiRouter.delete();
+
+apiRouter.delete("/api/notes/:id", (req, res) => {
+    //filter method by ID, create a new array without that ID
+    fs.readFile('./db/db.json', 'utf8', (error, file) => {
+        if (error) throw error;
+
+        // deleted note id
+        let deletedNoteId = req.params.id;
+
+        // parse db.json into a JSON object
+        const parsedFile = JSON.parse(file);
+
+        // filter method by ID, create a new array without that ID
+        const newParsedFile = parsedFile.filter(elem => elem.id != deletedNoteId);
+
+        //Create new stringify of the combined file to write back to file
+        const newStringifiedFile = JSON.stringify(newParsedFile);
+
+        // re-write the file as the combined file
+        fs.writeFile('./db/db.json', newStringifiedFile, 'utf8', (err) => {
+            if (err) throw err;
+            console.log("The note was deleted!");
+        });
+        
+        // return parsed version of new stringified file to read back to the page
+        return res.send(JSON.parse(newStringifiedFile));
+    });
+});
 
 module.exports = apiRouter;
 
